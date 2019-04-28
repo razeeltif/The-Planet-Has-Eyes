18d8e73a2eb00a9b3bc18e9999de7b59d6788949
AsbelGrants <loic.dasilva@gmail.com>
2019-04-28 20:09:55 +0200

    n

---
+++ Assets/scripts/VideoHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoHandler : MonoBehaviour
+{
+	VideoPlayer player;
+
+	bool isSkiping;
+
+    void Awake()
+    {
+		StartCoroutine(PlayIntro());
+    }
+
+	private void Update()
+	{
+		if(!isSkiping && (Input.GetKeyDown(KeyCode.Space)))
+		{
+			StartCoroutine(FadeOut());
+		}
+		else if (isSkiping)
+		{
+			player.SetDirectAudioVolume(0, player.GetDirectAudioVolume(0) - 0.02f);
+		}
+	}
+
+	IEnumerator PlayIntro()
+	{
+		player.Play();
+		yield return new WaitForSeconds(0.5f);
+		yield return new WaitUntil(() => player.isPlaying == false);
+		SceneManager.LoadScene(2);
+	}
+
+	IEnumerator FadeOut()
+	{
+		isSkiping = true;
+		GetComponent<Animation>().Play();
+		yield return new WaitForSeconds(0.1f);
+		yield return new WaitUntil(() => GetComponent<Animation>().isPlaying == false);
+		SceneManager.LoadScene(1);
+	}
+}
