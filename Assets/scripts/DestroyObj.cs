cb58646398858a2e9e79d3d2088ba5a832046ec6
AsbelGrants <loic.dasilva@gmail.com>
2019-04-28 17:14:50 +0200

    Plein de choses

---
+++ Assets/scripts/DestroyObj.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObj : MonoBehaviour {
+
+    public GameObject Texte;
+
+    void Start()
+    {
+        StartCoroutine(RemoveAfterSeconds(6, Texte));
+    }
+    IEnumerator RemoveAfterSeconds(int seconds, GameObject obj)
+    {
+        yield return new WaitForSeconds(6);
+        obj.SetActive(false);
+    }
+
+}
